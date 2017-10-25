39700f3ef48321ff78e2cf6c4ae74f969690cb3e
Satyendra07 <satyendrayadav29@gmail.com>
2017-10-25 21:41:22 +0530

    promises 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -16,7 +16,7 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
     
-    this.leader = this.leaderservice.getLeaders();
+    this.leaderservice.getLeaders().then(leader => this.leader = leader);
   }
 
 }
